5a0004259dbb42018d031dbd7f246bdabf050560
IamtheTDogg <61993152+IamtheTDogg@users.noreply.github.com>
2020-06-16 10:11:30 +1200

    Add files via upload

--- Polluted2__v5.py
+++ Polluted2__v5.py
@@ -46,8 +46,17 @@ def press(button):
 
 # choices
 
-choices.append(Choices("you walk towards the mountain","1: climb over it","2: walk around it",1,0))
-choices.append(Choices("you reach the top of the mountain and find a cave","1:  walk inside","2: go down the other side of the mountain",0,0))
+choices.append(Choices("you walk towards the mountain","1: climb over it","2: walk around it",1,6))
+choices.append(Choices("you reach the top of the mountain and find a cave","1:  walk inside","2: go down the other side of the mountain",2,9))
+choices.append(Choices("you walk into the cave and find corona fight or run","1: fight","2: run",3,10))
+choices.append(Choices("you try to fight corona but corona gives you a cold what do you do","1: live out your last days","2: keep fighting",4,11))
+choices.append(Choices("you find a nice trash cottage live there or keep moving","1: live there","2: keep moving",5,13))
+choices.append(Choices("as you are dying you play animal crossing to die happily","1: restart","2: restart",0,0))
+choices.append(Choices("you walk around it and find a hobo trying to sell shrooms","1: buy them","2: keep going",7,16))
+choices.append(Choices("you buy the shrooms and he tells you not to take all of them at once","1: take them all at once","2: listen to him",8,21))
+choices.append(Choices("you take all of them have two secounds of fun then your brain explodes","1: restart","2: restart",0,0))
+choices.append(Choices("you slip and fall to your death","1: restart","2: restart",0,0))
+choices.append(Choices("you try to run but corona throws its bottle cap at you and you get beheaded","1: restart","2: restart",0,0))
 
 
 # create a GUI variable called app
